77dd621611093d9614204d69bfbb5a481eda0c1c
João Bosco Dos Reis Becker <joao.jbosco@gmail.com>
2024-01-11 09:31:57 -0300

    Adicionar arquivos de projeto.

---
+++ Entidade/Produto.cs
@@ -0,0 +1,33 @@
+﻿using System.Globalization;
+
+namespace ConsolePOOProduto.Entidade
+{
+  internal class Produto
+  {
+    public string Nome= "";
+    public double Preco;
+    public int quantidade;
+
+    public double ValorTotalEmEstoque() {
+      return this.quantidade * this.Preco;
+    }
+
+    public void AdicionarProdutos(int quantity ) {
+      this.quantidade += quantity;
+    }
+
+    public void RemoverProdutos(int quantity) {
+      this.quantidade -= quantity;
+    }
+
+    public override string ToString()
+    {
+      return this.Nome + ", $" +
+             this.Preco.ToString("F2", CultureInfo.InvariantCulture) + ", " +
+             this.quantidade + " unidades, Total: " +
+             this.ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+
+    }
+
+  }
+  }
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsolePOOProduto.Entidade;
+using System.Globalization;
+
+Console.Write("Entre com  os dados do produto: ");
+Produto produto = new Produto();
+Console.Write("Nome:");
+produto.Nome = Console.ReadLine();
+Console.Write("Preço: ");
+produto.Preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+Console.Write("Quantidade no estoque: ");
+produto.quantidade = int.Parse(Console.ReadLine());
+
+/// Esse é o  exemplo fazendo o override no método virtual ToString();
+Console.Write("Dados do produto: " + produto);
+
+//Console.WriteLine("Dados do produto: "+ produto.Nome + " R$"+produto.Preco+ " Quantidade:"+produto.ValorTotalEmEstoque()+ " Total :"+(produto.quantidade*produto.Preco));
+/*Console.WriteLine("Dados do produto: " + produto);
+Console.WriteLine("Digite o numero de produtos a ser adicionado no estoque: ");
+produto.AdicionarProdutos(int.Parse(Console.ReadLine()));
+Console.WriteLine("Dados Atualizados " + produto.Nome + " R$" + produto.Preco + " Quantidade:" + produto.ValorTotalEmEstoque() + " Total :" + (produto.quantidade * produto.Preco));
+Console.WriteLine("Digite  o Numero de produtos a ser removido do estoque ");
+produto.RemoverProdutos(int.Parse(Console.ReadLine()));
+Console.WriteLine("Dados Atualizados " + produto.Nome + " R$" + produto.Preco + " Quantidade:" + produto.ValorTotalEmEstoque() + " Total :" + (produto.quantidade * produto.Preco));
+*/
